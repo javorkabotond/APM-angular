8831f90e9d602db2fcacd93c34c60e7639dab29b
javorkabotond <botondjvorka@gmail.com>
2020-07-23 15:45:09 +0200

    add star component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -84,6 +84,9 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     console.log('In OnInit');
   }
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
